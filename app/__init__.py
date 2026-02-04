04b6e065a17e89c39a209186eec5c7600df126d3
raydeveloppeur-admin <raydeveloppeur@gmail.com>
2026-02-05 00:29:49 +0100

    Create login feature with password authentication in the controller : Change the variable name and initialize its value inside if __name__ == "main" to improve readability

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from app.configuration_variables import ConfigurationVariables
 from app.controllers.auth import auth_bp
 
 
-def create_app():
+def initialize_flask_application():
     app = Flask(__name__)
     app.secret_key = ConfigurationVariables.SECRET_KEY
     app.register_blueprint(auth_bp)
