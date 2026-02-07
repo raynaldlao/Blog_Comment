d693d6762ec88fd28aa9bc5d792bea77070431a8
raydeveloppeur-admin <raydeveloppeur@gmail.com>
2026-02-07 19:07:51 +0100

    Create login feature with password authentication in the controller : Create a Python file outside the MVC architecture to connect to the production or test database. Add robust detection of pytest execution using PYTEST_CURRENT_TEST or 'pytest' in sys.modules to automatically switch to the test database in database.py and __init__.py.

--- app/database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-
-from app.configuration_variables import ConfigurationVariables
-
-database_engine  = create_engine(ConfigurationVariables.DATABASE_URL)
-Base = declarative_base()
