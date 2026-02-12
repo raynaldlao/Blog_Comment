5a7b70ae3ab07738181dac99e977bc19f6314dc7
largittehuna <largitte.huna@gmail.com>
2026-02-12 21:44:11 +0100

    Manage articles, comments and replies :
    + Replaced the old login‑based home page with a new homepage displaying the list of blog articles, and added a dedicated tab to access the login page.
    
    + Updated role permissions so admins and authors can create, read and update articles, while users and non‑authenticated visitors have read‑only access.
    
    + Admins can delete all articles, while authors can only delete their own articles.
    
    + All authenticated roles can post comments, and the admin can delete any comment.

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ import sys
 
 from flask import Flask
 
+from app.controllers.article import article_bp
 from app.controllers.login import login_bp
 from configurations.configuration_variables import env_vars
 
@@ -14,4 +15,5 @@ def initialize_flask_application():
     else:
         app.secret_key = env_vars.secret_key
     app.register_blueprint(login_bp)
+    app.register_blueprint(article_bp)
     return app
