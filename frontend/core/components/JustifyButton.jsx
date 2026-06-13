dcf05276973677205427a81fc4634799d8c54b19
raydeveloppeur-admin <raydeveloppeur@gmail.com>
2026-06-14 01:34:14 +0200

    Notion style editor : This update aligns the JustifyButton with the toolbar’s ActionIcon style by switching to an inline SVG, renaming the tooltip to “Align text justify” and passing the icon through the `icon` prop so BlockNote renders it as a proper MantineActionIcon

--- frontend/core/components/JustifyButton.jsx
+++ frontend/core/components/JustifyButton.jsx
@@ -19,7 +19,7 @@ export default function JustifyButton() {
 
   return (
     <Components.FormattingToolbar.Button
-      mainTooltip="Justify"
+      mainTooltip="Align text justify"
       onClick={() => {
         for (const block of blocks) {
           if (block.props?.textAlignment !== undefined) {
@@ -30,13 +30,19 @@ export default function JustifyButton() {
         }
       }}
       isSelected={isSelected}
-    >
-      <span
-        className="material-symbols-outlined"
-        style={{ fontSize: '1.125rem' }}
-      >
-        format_align_justify
-      </span>
-    </Components.FormattingToolbar.Button>
+      icon={
+        <svg
+          stroke="currentColor"
+          fill="currentColor"
+          strokeWidth="0"
+          viewBox="0 0 24 24"
+          height="1em"
+          width="1em"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M3 4H21V6H3V4ZM3 19H21V21H3V19ZM3 14H21V16H3V14ZM3 9H21V11H3V9Z" />
+        </svg>
+      }
+    />
   );
 }
